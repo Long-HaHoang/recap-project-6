630680ea90ebe3df7da4c649c474b3f802772333
LongHaHoang <longhahoang1992@gmail.com>
2023-02-13 02:10:31 +0100

    init commit

---
+++ helpers/db.js
@@ -0,0 +1,67 @@
+import { v4 as uuidv4 } from "uuid";
+import mongoose, { model, models, Schema } from "mongoose";
+
+const URI = `mongodb+srv://Viper6389:<password>@cluster0.iph03jw.mongodb.net/test?retryWrites=true&w=majority`;
+
+const placeSchema = new Schema(
+  {
+    id: String,
+    location: String,
+    image: String,
+    mapURL: String,
+    description: String,
+    name: String,
+  },
+  {
+    collection: "attractions",
+  }
+);
+
+const Place = models.Place || model("Place", placeSchema);
+
+async function connectDatabase() {
+  await mongoose.connect(URI);
+}
+
+async function getAllPlaces() {
+  await connectDatabase();
+
+  const places = await Place.find({});
+
+  return places;
+}
+
+async function getPlace(id) {
+  await connectDatabase();
+  const place = await Place.findOne({ id });
+  return place;
+}
+
+async function createPlace(place) {
+  await connectDatabase();
+
+  const createdPlace = await Place.create({
+    ...place,
+    id: uuidv4(),
+  });
+  return createdPlace;
+}
+
+async function deletePlace(id) {
+  await connectDatabase();
+  const deletedPlace = getPlace(id);
+  await Place.deleteOne({ id });
+  return deletedPlace;
+}
+
+async function updatePlace(id, place) {
+  await connectDatabase();
+
+  await Place.updateOne({ id }, place);
+
+  const updatedPlace = getPlace(id);
+
+  return updatedPlace;
+}
+
+export { getAllPlaces, getPlace, createPlace, deletePlace, updatePlace };
---
+++ pages/create.js
@@ -0,0 +1,32 @@
+import Link from "next/link.js";
+import styled from "styled-components";
+import { useRouter } from "next/router";
+import Form from "../components/Form.js";
+import { StyledLink } from "../components/StyledLink.js";
+
+const StyledBackLink = styled(StyledLink)`
+  justify-self: flex-start;
+`;
+
+export default function CreatePlacePage() {
+  const router = useRouter();
+
+  async function addPlace(place) {
+    await fetch("api/places", {
+      method: "POST",
+      body: JSON.stringify(place),
+    });
+
+    router.push("/");
+  }
+
+  return (
+    <>
+      <h2 id="add-place">Add Place</h2>
+      <Link href="/" passHref legacyBehavior>
+        <StyledBackLink>back</StyledBackLink>
+      </Link>
+      <Form onSubmit={addPlace} formName={"add-place"} />
+    </>
+  );
+}
